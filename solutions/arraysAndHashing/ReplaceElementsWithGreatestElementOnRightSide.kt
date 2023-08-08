c33117593e3d12672af65ce57ab5b8d0f5eb8ba8
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2023-08-08 17:28:32 -0400

    Create ReplaceElementsWithGreatestElementOnRightSide.kt

---
+++ solutions/arraysAndHashing/ReplaceElementsWithGreatestElementOnRightSide.kt
@@ -0,0 +1,15 @@
+package solutions.arraysAndHashing
+
+fun replaceElements(arr: IntArray): IntArray {
+    var max = arr[arr.lastIndex]
+
+    for (i in arr.size - 2 downTo 0) {
+        val temp = arr[i]
+        arr[i] = max
+        max = maxOf(max, temp)
+    }
+
+    arr[arr.lastIndex] = -1
+
+    return arr
+}
