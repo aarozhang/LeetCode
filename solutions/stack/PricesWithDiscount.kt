cb3f0ca90a26a3a18878064ea14947a59fd6d4d1
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2023-09-08 19:25:24 -0400

    Create PricesWithDiscount.kt

---
+++ solutions/stack/PricesWithDiscount.kt
@@ -0,0 +1,28 @@
+package solutions.stack
+
+import java.util.*
+
+// O(n) time, space
+fun finalPrices(prices: IntArray): IntArray {
+    val stack = Stack<Int>()
+    val res = IntArray(prices.size) { 0 }
+
+    stack.push(prices[prices.size - 1])
+    res[res.size - 1] = stack.peek()
+
+    for (i in prices.size - 2 downTo 0) {
+        while (!stack.empty() && stack.peek() > prices[i]) {
+            stack.pop()
+        }
+
+        if (stack.empty()) {
+            res[i] = prices[i]
+        } else {
+            res[i] = prices[i] - stack.peek()
+        }
+
+        stack.push(prices[i])
+    }
+
+    return res
+}
