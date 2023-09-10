3d0bd4eb96901dd0ca517a6a6ea9a5632671f41f
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2023-09-10 14:49:54 -0400

    Create BasicCalculatorII.kt

---
+++ solutions/stack/BasicCalculatorII.kt
@@ -0,0 +1,37 @@
+package solutions.stack
+
+import java.util.*
+
+fun calculate(s: String): Int {
+    val stack = Stack<Int>()
+    var currNumber = 0
+    var operation = '+'
+    var i = 0
+
+    for (c in s) {
+        if (c.isDigit()) {
+            currNumber = (currNumber * 10) + (c - '0')
+        }
+
+        if (!c.isDigit() && !c.isWhitespace() || i == s.length - 1) {
+            when (operation) {
+                '+' -> stack.push(currNumber)
+                '-' -> stack.push(-currNumber)
+                '*' -> stack.push(stack.pop() * currNumber)
+                '/' -> stack.push(stack.pop() / currNumber)
+            }
+
+            operation = c
+            currNumber = 0
+        }
+
+        i += 1
+    }
+
+    var res = 0
+    for (n in stack) {
+        res += n
+    }
+
+    return res
+}
