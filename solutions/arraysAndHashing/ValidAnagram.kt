80e1c47cfe6b777e4df5f827c1f0295e0a9f1b83
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2023-10-06 20:15:16 -0400

    Update ValidAnagram.kt

--- solutions/arraysAndHashing/ValidAnagram.kt
+++ solutions/arraysAndHashing/ValidAnagram.kt
@@ -1,18 +1,37 @@
 package solutions.arraysAndHashing
 
+// O(n) time & space
+// approach using array as hash map
 fun isAnagram(s: String, t: String): Boolean {
-    val counts = Array(26) {0}
+    if (s.length != t.length) return false
 
-    for (c in s) {
-        counts[c - 'a'] += 1
-    }
+    val counts = Array(26) { 0 }
 
-    for (c in t) {
-        counts[c - 'a'] -= 1
+    for (i in s.indices) {
+        counts[s[i] - 'a'] += 1
+        counts[t[i] - 'a'] -= 1
     }
 
     for (n in counts) {
         if (n != 0) return false
+    }
+
+    return true
+}
+
+// Approach using hashmap
+fun isAnagramHashMap(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val map = mutableMapOf<Char, Int>()
+
+    for (i in s.indices) {
+        map[s[i]] = map.getOrDefault(s[i], 0) + 1
+        map[t[i]] = map.getOrDefault(t[i], 0) - 1
+    }
+
+    for (v in map.values) {
+        if (v != 0) return false
     }
 
     return true
